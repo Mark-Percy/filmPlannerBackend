c55ac53afa63929fbb59f0a2996c696e0e0ef3e7
Mark Percy <mark.percy98@outlook.com>
2023-11-23 16:46:06 +0000

    fix issues for linting

--- functions/index.js
+++ functions/index.js
@@ -18,31 +18,27 @@ const tmdb = defineSecret('tmdbkey');
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
 
-exports.getFilms = onRequest({ secrets: [tmdb] },(request, response) => {
-	if(request.query.dateFrom) logger.info(request.query.dateFrom)
-	else response.send('No Date Selected')
-	const fromDate = new Date(request.query.dateFrom)
-	const toDate = new Date(fromDate)
-	toDate.setMonth(toDate.getMonth() + 1)
-	logger.info(fromDate.toISOString().split('T')[0])
-	logger.info(toDate.toISOString().split('T')[0])
+exports.getFilms = onRequest({secrets: [tmdb]}, (request, response) => {
+	if (request.query.dateFrom) logger.info(request.query.dateFrom);
+	else response.send('No Date Selected');
+	const fromDate = new Date(request.query.dateFrom);
+	const toDate = new Date(fromDate);
+	toDate.setMonth(toDate.getMonth() + 1);
 	logger.info('Getting Films from tmdb from date');
 	const url = `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-UK&page=1&primary_release_date.gte=${fromDate.toISOString().split('T')[0]}&primary_release_date.lte=${toDate.toISOString().split('T')[0]}&sort_by=popularity.desc&with_release_type=3&region=GB`;
 	const options = {
 		method: 'GET',
 		headers: {
 			accept: 'application/json',
-			Authorization: tmdb.value()
-		}
+			Authorization: tmdb.value(),
+		},
 	};
-	
+
 	fetch(url, options)
-		.then(res => res.json())
-		.then(json => {
+		.then((res) => res.json())
+		.then((json) => {
 			response.send(json);
 		})
-		.catch(err => console.error('error:' + err));
-	// response.send(request.query.dateFrom);
-
+		.catch((err) => console.error('error:' + err));
 });
 
